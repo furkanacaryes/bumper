12eac9e5f556020f8171b9593d0ea7c823454568
furkanacaryes <m.furkanacar@icloud.com>
2025-07-15 15:08:08 +0300

    fix(RootLayout): adds top-level query client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,48 @@
-import type { Metadata } from "next";
+"use client";
+
+import { useState } from 'react';
+// import type { Metadata } from "next";
 import { Oswald, Open_Sans } from "next/font/google";
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import "./globals.css";
 
-export const openSans = Open_Sans({
+const openSans = Open_Sans({
   subsets: ["latin"],
   variable: "--font-open-sans",
   display: "swap",
 });
 
-export const oswald = Oswald({
+const oswald = Oswald({
   subsets: ["latin"],
   variable: "--font-oswald",
   display: "swap",
 });
 
-export const metadata: Metadata = {
-  title: "Bumper",
-  description: "Give customers more flexibility at checkout",
-};
+// export const metadata: Metadata = {
+//   title: "Bumper",
+//   description: "Give customers more flexibility at checkout",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <html lang="en">
-      <body
-        className={`${openSans.variable} ${oswald.variable} antialiased`}
-      >
-        {children}
+      <body className={`${openSans.variable} ${oswald.variable} antialiased`}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+
+          {process.env.NODE_ENV !== 'production' && (
+            <ReactQueryDevtools initialIsOpen={false} />
+          )}
+        </QueryClientProvider>
       </body>
     </html>
   );
