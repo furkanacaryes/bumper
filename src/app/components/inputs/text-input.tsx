e728a6f34892a6dcfec9bd3d27b3036d999d9dad
furkanacaryes <m.furkanacar@icloud.com>
2025-07-15 15:05:12 +0300

    fix(TextInput): adds blur handler and form props

--- src/app/components/inputs/text-input.tsx
+++ src/app/components/inputs/text-input.tsx
@@ -3,14 +3,18 @@ import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export type TextInputProps = {
+	name?: string;
 	label?: string;
+	placeholder?: string;
 	value?: string;
-	onChange: VoidFunction;
+	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
 	icon?: IconDefinition;
 	required?: boolean;
+	error?: string | false | undefined;
 }
 
-export const TextInput: FC<TextInputProps> = ({ label, icon, value, onChange }) => {
+export const TextInput: FC<TextInputProps> = ({ name, label, placeholder, icon, value, onChange, onBlur, error }) => {
 	return (
 		<div className="input flex flex-col gap-2">
 			<div className="input-label space-x-2 text-base font-bold">
@@ -22,8 +26,9 @@ export const TextInput: FC<TextInputProps> = ({ label, icon, value, onChange }) 
 			</div>
 
 			<div className="input-control border border-brand-darkgrey rounded-full w-full py-2 px-4">
-				<input type="text" className="w-full focus:outline-none" value={value} onChange={onChange} />
+				<input name={name} type="text" placeholder={placeholder} className="w-full focus:outline-none" value={value} onChange={onChange} onBlur={onBlur} />
 			</div>
+			{error && <div className="text-red-500 text-sm">{error}</div>}
 		</div>
 	)
 }
