ca15ae194b487ecaa7c27204ac3908e18f3731f9
furkanacaryes <m.furkanacar@icloud.com>
2025-07-15 11:07:27 +0300

    feat: adds oswald, open sans fonts, brand colors

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line
+export default {
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          orange: "#FF733C",
+          darkgrey: "#1B1B1B",
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-open-sans)', 'sans-serif'],
+        oswald: ['var(--font-oswald)', 'sans-serif'],
+      },
+    },
+  },
+};
