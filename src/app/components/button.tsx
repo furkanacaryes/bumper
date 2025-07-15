190af546d831257c4cb17b28c6fcf810ee6f2bf6
furkanacaryes <m.furkanacar@icloud.com>
2025-07-15 11:10:08 +0300

    feat: adds `Button` component

---
+++ src/app/components/button.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+
+export type ButtonProps = {
+	text: string;
+	onPress: VoidFunction;
+	className?: string;
+}
+
+export const Button: FC<ButtonProps> = ({ text, onPress, className }) => {
+	return (<button className={`flex justify-between items-center gap-4 bg-brand-accent text-brand-darkgrey text-base px-6 py-3 rounded-full border border-brand-darkgrey hover:bg-brand-accent hover:text-white active:bg-brand-darkgrey active:text-white ${className}`} onClick={onPress}>
+		<span>{text}</span>
+		<FontAwesomeIcon icon={faArrowRightLong} className="text-inherit" width={16} />
+	</button>)
+}
